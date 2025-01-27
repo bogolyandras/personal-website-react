cdb5a7a933ce3940d572959e0b5dd5d23f884d80
András Bögöly <andras.bogoly.91@gmail.com>
2025-01-27 23:55:53 +0100

    Give up on that idea

--- react-snap.config.js
+++ react-snap.config.js
@@ -3,10 +3,7 @@ const config = {
     '/',
     '/blog/',
     '/world-news/',
-  ],
-  puppeteerArgs: process.env.CI 
-    ? ['--no-sandbox', '--disable-setuid-sandbox']
-    : []
+  ]
 };
 
 module.exports = config;
